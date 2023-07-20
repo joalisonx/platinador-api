1b214d958a69d52cb3e05105b125e475bd96be5a
unknown <me.joalison.sousa007@gmail.com>
2023-07-20 17:00:47 -0300

    Agora as consultas no banco de dados são realizadas a cada 2 minutos, ao invés de uma a cada request recebido

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,10 +3,20 @@ import { MongoClient } from "mongodb";
 import { IGame } from "../utils";
 
 const client = new MongoClient(process.env.DB_URL);
-const db = client.db(process.env.DB_NAME).collection<IGame>(process.env.DB_COLLECTION);
+const db = client
+  .db(process.env.DB_NAME)
+  .collection<IGame>(process.env.DB_COLLECTION);
 
 async function getGames(): Promise<IGame[]> {
   return db.find().toArray() as unknown as IGame[];
 }
 
-export { getGames }
+let games: IGame[] = [];
+setInterval(async () => {
+  games = await getGames();
+}, 2 * 60000);
+
+export { 
+  getGames, 
+  games 
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router, Request, Response } from "express";
-import { getGames } from "../db";
+import { getGames, games } from "../db";
 
 export const index = Router();
 
 index.get("/", async (req: Request, res: Response): Promise<void> => {
   res.set({"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
-  res.json(JSON.stringify(await getGames()));
+  res.json(JSON.stringify(games.length ? games : await getGames())).status(200);
 });
